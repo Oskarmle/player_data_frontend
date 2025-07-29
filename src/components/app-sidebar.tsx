ff5c3eb4d070bee62cfb47295f6e1a0c02539024
OskarEriksen <omle.eriksen@gmail.com>
2025-07-29 11:07:22 +0200

    Initial setup and basic menu styling

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,50 @@
+import { Home, User } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Hjem",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Din profil",
+    url: "#",
+    icon: User,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
